4eb35f4b0befe9f42b7963d15f81d8c14195ff02
Pierre Cabrière <hello@pierrecabriere.fr>
2020-03-31 00:17:22 +0200

    fixes & improvements

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,9 @@ interface IOptions {
 }
 
 const getDiffPaths = (object, base, path = []) => {
-  return Object.keys(object).reduce((result, key) => {
+  let keys = Object.keys(object);
+  keys = keys.concat(Object.keys(base).filter(key => !keys.includes(key)));
+  return keys.reduce((result, key) => {
     const to = object && object[key] && mongoose.Types.ObjectId.isValid(object[key]) ? object[key].toString() : object[key];
     const from = base && base[key] && mongoose.Types.ObjectId.isValid(base[key]) ? base[key].toString() : base[key];
 
